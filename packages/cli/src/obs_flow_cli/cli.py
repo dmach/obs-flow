3253566be4b0192947843bcdb1c314743670ad97
Daniel Mach <daniel.mach@suse.com>
2026-08-13 16:24:36 +0200

    Show tracebacks caused by HTTPError only if --traceback is specified

--- packages/cli/src/obs_flow_cli/cli.py
+++ packages/cli/src/obs_flow_cli/cli.py
@@ -8,6 +8,7 @@ from .lazy_group import LazyGroup
 
 @click.group(cls=LazyGroup, context_settings={"help_option_names": ["-h", "--help"]})
 @click.option("-v", "--verbose", is_flag=True, help="Enable verbose output")
+@click.option("--traceback", is_flag=True, help="Show full traceback on error")
 @click.pass_context
 def main(ctx: click.Context, verbose: bool, traceback: bool) -> None:
     """
@@ -15,12 +16,23 @@ def main(ctx: click.Context, verbose: bool, traceback: bool) -> None:
     """
     ctx.ensure_object(dict)
     ctx.obj["verbose"] = verbose
+    ctx.obj["traceback"] = traceback
     if verbose:
         os.environ["OBS_FLOW_VERBOSE"] = "1"
+    if traceback:
+        os.environ["OBS_FLOW_TRACEBACK"] = "1"
 
 
 def run() -> None:
     """
     Entrypoint that runs the main CLI.
     """
-    main()
+    from requests.exceptions import HTTPError
+
+    try:
+        main()
+    except HTTPError as e:
+        if "--traceback" in sys.argv or os.environ.get("OBS_FLOW_TRACEBACK") == "1":
+            raise
+        print(str(e), file=sys.stderr)
+        sys.exit(1)
