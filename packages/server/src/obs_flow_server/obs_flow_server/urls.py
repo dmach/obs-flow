13f24eb2e1f162e7989136b7a6d7347fb2a8625a
Daniel Mach <daniel.mach@suse.com>
2026-09-15 08:57:34 +0200

    Change /pull_requests/ url path to /pull-requests/

--- packages/server/src/obs_flow_server/obs_flow_server/urls.py
+++ packages/server/src/obs_flow_server/obs_flow_server/urls.py
@@ -26,8 +26,8 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", core_views.home, name="home"),
     path("git-mappings/", core_views.git_mapping_list, name="git_mapping_list"),
-    path("pull_requests/", pr_views.pr_list, name="pr_list"),
-    path("pull_requests/<int:pr_id>/", pr_views.pr_detail, name="pr_detail"),
+    path("pull-requests/", pr_views.pr_list, name="pr_list"),
+    path("pull-requests/<int:pr_id>/", pr_views.pr_detail, name="pr_detail"),
     path("staging/", staging_views.staging_list, name="staging_list"),
     path("staging/<int:batch_id>/", staging_views.staging_detail, name="staging_detail"),
     path(
